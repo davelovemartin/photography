e6ee21299c3e18f181417d1e3f1a05d8af54c86a
justan0therdave <justanotherdavemartin@gmail.com>
2018-01-17 11:58:54 +0000

    added pages and template files

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/about/">about</Link>
+    <Link to="/contact/">contact</Link>
   </div>
 )
 
