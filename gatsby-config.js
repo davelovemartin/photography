d05f4ea714f73c33f85cbce7f7604c9a42a41a00
justan0therdave <justanotherdavemartin@gmail.com>
2018-01-20 18:01:55 +0000

    added netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
-    }
+    },
+    `gatsby-plugin-netlify`
   ]
 };
