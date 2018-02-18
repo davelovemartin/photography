072457f63f8a0172dd794bc4daeb545df05ccbdc
justan0therdave <justanotherdavemartin@gmail.com>
2018-02-18 20:28:51 +0000

    added paypal and orders

--- src/templates/product.js
+++ src/templates/product.js
@@ -103,12 +103,14 @@ class PayPalForm extends React.Component {
       paypal = require('paypal-checkout');
     }
     let PayPalButton = paypal.Button.driver('react', { React, ReactDOM });
-
+    let client = {
+      sandbox: process.env.PAYPAL_CLIENT_ID
+    }
     return (
       <div className='shoppingCart'>
         <PayPalButton
           env={'sandbox'}
-          client={{sandbox: process.env.PAYPAL_CLIENT_ID}}
+          client={client}
           payment={ (data, actions) => this.payment(data, actions) }
           commit={true}
           onAuthorize={ (data, actions) => this.onAuthorize(data, actions) }
