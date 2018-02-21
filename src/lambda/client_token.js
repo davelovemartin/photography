3eae06e5fd01ee6bfdcb78beea9b4203a80fc19f
justan0therdave <justanotherdavemartin@gmail.com>
2018-02-21 18:42:24 +0000

    added paypal and orders

--- src/lambda/client_token.js
+++ src/lambda/client_token.js
@@ -4,16 +4,28 @@ var gateway = braintree.connect({
 });
 
 exports.handler = function(event, context, callback) {
-    return gateway.clientToken.generate({}).then(result => {
+    return gateway.clientToken.generate({})
+    .then(result => {
         const response = {
             statusCode: 200,
             headers: {
               'Access-Control-Allow-Origin': '*'
             },
-            body: JSON.stringify(
-                {clientToken: result.clientToken}
-            )
+            body: JSON.stringify({clientToken: result.clientToken})
         }
         callback(null, response)
-    });
+    })
+    .catch((err) => {
+        console.log(err)
+        const response = {
+          statusCode: 500,
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify({
+            error: err.message
+          })
+        }
+        callback(null, response)
+      });
 };
