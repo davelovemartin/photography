a0ceb690a769a0a4934f7d9712e4523e2641dc4b
justan0therdave <justanotherdavemartin@gmail.com>
2018-02-21 22:44:56 +0000

    added paypal and orders

--- src/lambda-src/client_token.js
+++ src/lambda-src/client_token.js
@@ -1,34 +1,30 @@
-// const braintree = require('braintree');
+const braintree = require('braintree');
+const gateway = braintree.connect({ accessToken: process.env.BRAINTREE_TOKEN });
 
 exports.handler = function(event, context, callback) {
-  // const gateway = braintree.connect({ accessToken: process.env.BRAINTREE_TOKEN });
-  console.log("hi");
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
-    // return gateway.clientToken.generate({})
-    // .then(result => {
-    //     const response = {
-    //         statusCode: 200,
-    //         headers: {
-    //           'Access-Control-Allow-Origin': '*'
-    //         },
-    //         body: JSON.stringify({clientToken: result.clientToken})
-    //     }
-    //     callback(null, response)
-    // })
-    // .catch((err) => {
-    //   console.log(err)
-    //   const response = {
-    //     statusCode: 500,
-    //     headers: {
-    //       'Access-Control-Allow-Origin': '*'
-    //     },
-    //     body: JSON.stringify({
-    //       error: err.message
-    //     })
-    //   }
-    //   callback(null, response)
-    // });
+  
+    return gateway.clientToken.generate({})
+    .then(result => {
+        const response = {
+            statusCode: 200,
+            headers: {
+              'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({clientToken: result.clientToken})
+        }
+        callback(null, response)
+    })
+    .catch((err) => {
+      console.log(err)
+      const response = {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          error: err.message
+        })
+      }
+      callback(null, response)
+    });
 }
